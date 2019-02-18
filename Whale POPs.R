9927e16953ca9f0946756f3445a8e7eacf5cacca
Matthew Savoca <msavoca13@gmail.com>
2019-02-18 10:32:07 -0800

    adding in trophic level from Pauly et al. 1998

--- Whale POPs.R
+++ Whale POPs.R
@@ -39,7 +39,7 @@ p1 <- ggplot(data = filter(Whale_POPs, `Pollutant type`!= "Hg"), aes(Basin, log1
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 p1
 
-#Graoh pollutant by prey
+#Graph pollutant by prey
 Whale_POPs$`Prey type` <- revalue(Whale_POPs$`Prey type`, c("Zooplankton"="Crustacean", "Krill"="Crustacean"))
 Whale_POPs$`Prey type` <- fct_relevel(Whale_POPs$`Prey type`, "Top predator", "Fish", "Cephalopods", "Forage fish", "Crustacean")
 # vline.dat <- data.frame(`Pollutant type`=levels(Whale_POPs$`Pollutant type`), vl=c(4,3.17,4))
@@ -53,6 +53,16 @@ p2 <- ggplot(data = filter(Whale_POPs, `Prey type` != "NA" & `Pollutant type` !=
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 p2
 
+
+# Pollutant by trophic level (values from Pauly et al. 1998)
+p2_TL <- ggplot(data = filter(Whale_POPs, `Prey type` != "NA" & `Pollutant type` != "Hg"), aes(`Trophic level`, log10(`Average Pollutant load (ng/g lipid wt)`))) +
+  geom_point(aes(size = `Sample size`, color = Group), alpha = 0.3) +
+  geom_smooth(aes(weight = `Sample size`), method = "lm", color = "black") +
+  facet_grid(~`Pollutant type`) +
+  geom_hline(aes(yintercept=4), colour="#990000", linetype="dashed") +
+  xlab("Trophic level") + ylab("log[Average pollutant load (ng/g lipid wt)")+
+  theme_bw()
+p2_TL
 
 #pollutant by decade
 p3 <- ggplot(data = Whale_POPs, aes(`Collection decade`, log10(`Average Pollutant load (ng/g lipid wt)`))) +
